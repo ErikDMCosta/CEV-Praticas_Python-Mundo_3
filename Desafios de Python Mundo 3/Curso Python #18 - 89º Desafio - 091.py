b793d721bebee1227a605f0cdd8ed14e63e7b9dd
ErikDMCosta <53874072+ErikDMCosta@users.noreply.github.com>
2021-03-26 22:03:36 -0300

    Add arquivos dos Desafios do Mundo 3 via upload

---
+++ Desafios de Python Mundo 3/Curso Python #18 - 89º Desafio - 091.py
@@ -0,0 +1,24 @@
+ficha = list()
+pergunta = 'S'
+while pergunta != 'N':
+    nome = str(input('Nome: '))
+    ficha.append(nome)
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1 + nota2) / 2
+    ficha.append([nome, [nota1, nota2], media])
+    pergunta = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+print('-=' * 30)
+print(f'{"No.":<4}{"NOME":<10}{"MÉDIA":>8}')
+print('-' * 26)
+for i, a in enumerate(ficha):
+    print(f'{i}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    print('-' * 35)
+    opc = int(input('Mostrar notas de qual aluno? (999 interrompe): '))
+    if opc == 999:
+        print('FINALIZANDO...')
+        break
+    if opc <= len(ficha) - 1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
+print('<<< VOLTE SEMPRE >>>')
