49098d06401372626447b55c16a697443fb2961a
ErikDMCosta <53874072+ErikDMCosta@users.noreply.github.com>
2021-08-13 10:27:57 -0300

    Adicionei arquivos das Aulas do Mundo 3 via upload

---
+++ Aulas de Python Mundo 3/Curso Python - #20 - Funções - Part 1.py
@@ -0,0 +1,18 @@
+def lin():
+    print('-' * 30)
+
+# Programa Principal
+# print('-' * 30)
+lin()
+print(' CURSO EM VÍDEO ')
+# print('-' * 30)
+# print('-' * 30)
+lin()
+print(' APRENDA PYTHON ')
+# print('-' * 30)
+# print('-' * 30)
+lin()
+print(' GUSTAVO GUANABARA ')
+# print('-' * 30)
+# print('-' * 30)
+lin()
