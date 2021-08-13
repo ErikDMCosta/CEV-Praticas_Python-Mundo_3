f5e4fb6f5a4f74d3fa06b8c0433c96d9ecf02c60
ErikDMCosta <53874072+ErikDMCosta@users.noreply.github.com>
2021-08-13 10:28:36 -0300

    Adicionei arquivos das Aulas do Mundo 3 via upload

---
+++ Aulas de Python Mundo 3/Curso Python - #20 - Funções - Part 3.py
@@ -0,0 +1,31 @@
+def soma(a, b):
+    print(f'\n A = {a} e B = {b}')
+    s = a + b
+    print(f'A soma A + B = {s}')
+
+# Programa Principal
+soma(4, 5)
+soma(a=4, b=5)
+soma(b=4, a=5)
+soma(7, 2)
+# soma(3, 9, 5)
+soma(8, 9)
+soma(2, 1)
+soma(4, 1)
+
+"""
+a = 4
+b = 5
+s = a + b
+print(s)
+
+a = 8
+b = 9
+s = a + b
+print(s)
+
+a = 2
+b = 1
+s = a + b
+print(s)
+"""
