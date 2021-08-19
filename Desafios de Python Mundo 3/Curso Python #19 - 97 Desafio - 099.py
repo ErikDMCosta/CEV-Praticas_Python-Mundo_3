79667ffb3dcf73ce532cf5a29b5afafc3c142839
ErikDMCosta <53874072+ErikDMCosta@users.noreply.github.com>
2021-08-19 11:29:22 -0300

    Add arquivos dos Desafios do Mundo 3 via upload

---
+++ Desafios de Python Mundo 3/Curso Python #19 - 97 Desafio - 099.py
@@ -0,0 +1,8 @@
+def escreva(texto):
+    print('~' * (len(texto) + 4))
+    print(f'  {texto}')
+    print('~' * (len(texto) + 4))
+
+escreva('Olá, Mundo!')
+escreva('Curso em Python no YouTube')
+escreva('CeV')
