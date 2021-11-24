fe7d9e238dbc3ae0c6b36ac2563cac709b0266f4
ErikDMCosta <53874072+ErikDMCosta@users.noreply.github.com>
2021-11-24 14:42:07 -0300

    Delete Desafios de Python Mundo 3 directory

--- Desafios de Python Mundo 3/Curso Python #16 - 74º Desafio - 076.py
+++
@@ -1,7 +0,0 @@
-from random import randint
-escolhidos = (randint(1, 10)), (randint(1, 10)), (randint(1, 10)), (randint(1, 10)), (randint(1, 10))
-print(f'Os valores sorteados foram: ', end='')
-for numeros in escolhidos:
-    print(f'{numeros}', end=' ')
-print(f'\nO maior valor sorteado foi {max(escolhidos)}')# Método que funciona em Tuplas e outros lugares (mostra maior)
-print(f'O menor valor sorteado foi {min(escolhidos)}')# Método que funciona em Tuplas e outros lugares (mostra menor)
--- Desafios de Python Mundo 3/Curso Python #18 - 86º Desafio - 088.py
+++
@@ -1,9 +0,0 @@
-dados = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-for x in range(0, 3):
-    for y in range(0, 3):
-        dados[x][y] = int(input(f'Digite um valor para [{x}],[{y}]: '))
-print('-=' * 30)
-for x in range(0, 3):
-    for y in range(0, 3):
-        print(f'[{dados[x][y]:^5}]', end='')
-    print('')
